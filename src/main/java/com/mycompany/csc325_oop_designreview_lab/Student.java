3a3e81692954d1793a06d4c1b092ba6eefb1bfa2
Christopher Canenguez <canenguez.chris@gmail.com>
2022-09-28 10:22:20 -0400

    Ovverided the getter and setter methods of address in Student class.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -10,9 +10,21 @@ package com.mycompany.csc325_oop_designreview_lab;
  * @author MoaathAlrajab
  */
 public class Student extends Human{
-	// ToDo 1: Make this class a child of Human
+
+    @Override
+    public String getAddress() 
+    {
+        return this.address;
+    }
+
+    @Override
+    public void setAddress(String address) 
+    {
+        this.address = address;
+    }
+	// ToDo 1: Make this class a child of Human - Completed.
 	
-	// ToDo 2: Fix the resulting errors
+	// ToDo 2: Fix the resulting errors - Completed.
 	
 	// ToDo 3: Add a field for GPA and create setter and getter
 	
