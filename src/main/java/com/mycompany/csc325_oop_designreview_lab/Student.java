80669bf438eaa83aef58f9d0fef6af5d5d87ae1f
Christopher Canenguez <canenguez.chris@gmail.com>
2022-09-28 10:34:00 -0400

    Added Freshman and Senior classes.

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Student.java
@@ -5,10 +5,6 @@ package com.mycompany.csc325_oop_designreview_lab;
  *
  * @author MoaathAlrajab
  */
-/**
- *
- * @author MoaathAlrajab
- */
 public class Student extends Human
 {
 
@@ -21,7 +17,7 @@ public class Student extends Human
     public String getAddress() 
     {
         return this.address;
-    }
+    } // End getAddress.
 
     @Override
     /**
@@ -30,25 +26,25 @@ public class Student extends Human
     public void setAddress(String address) 
     {
         this.address = address;
-    }
+    } // End setAddress.
     
     /**
-     * getGpa - returns the Gpa of the Student.
+     * getGpa - returns the GPA of the Student.
      * @return int
      */
     public int getGpa() 
     {
         return gpa;
-    }
+    } // End getGpa.
 
     /**
-     * setGpa - updates the Gpa of the Student.
+     * setGpa - updates the GPA of the Student.
      * @param gpa 
      */
     public void setGpa(int gpa) 
     {
         this.gpa = gpa;
-    }
+    } // End setGpa.
 	// ToDo 1: Make this class a child of Human - Completed.
 	
 	// ToDo 2: Fix the resulting errors - Completed.
@@ -56,7 +52,4 @@ public class Student extends Human
 	// ToDo 3: Add a field for GPA and create setter and getter - Completed.
 	
 	// ToDo 4: Add comments to your code
-
-    
-
-}
+} // End Student.
